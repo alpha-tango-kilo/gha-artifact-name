84aa219e5e790a1f5f24e0aa945975fd0f534b47
alpha-tango-kilo <git@heyatk.com>
2024-02-14 13:09:11 +0000

    Test parseOverridesInput

--- src/main.test.ts
+++ src/main.test.ts
@@ -3,9 +3,8 @@ import * as core from "@actions/core";
 import { parseOverridesInput } from "./main";
 
 // Mock the GitHub Actions core library
-jest.mock("./index");
 jest.mock("@actions/core");
-jest.mock("@actions/github");
+// jest.mock("@actions/github");
 
 const getMultilineInputMock = <jest.Mock<typeof core.getMultilineInput>>(
     core.getMultilineInput
@@ -16,16 +15,50 @@ beforeEach(() => {
 });
 
 describe("parseOverridesInput", () => {
-    test("happy path", () => {
+    test("one", () => {
         getMultilineInputMock.mockImplementation((name: string): string[] => {
-            switch (name) {
-                case "overrides":
-                    return ["forgetfulness: forgor"];
-                default:
-                    throw new Error(`didn't expect getMultilineInput("${name}")`);
-            }
+            expect(name).toBe("overrides");
+            return ["forgetfulness: forgor"];
         });
 
-        expect(parseOverridesInput()).toEqual(new Map([["forgetfulness", "forgor"]]));
+        expect(parseOverridesInput()).toEqual(
+            new Map([["forgetfulness", "forgor"]]),
+        );
+    });
+
+    test("many", () => {
+        getMultilineInputMock.mockImplementation((name: string): string[] => {
+            expect(name).toBe("overrides");
+            return [
+                "forgetfulness: forgor",
+                "heehee: hoho",
+                "are you winning: son",
+            ];
+        });
+
+        const expected = new Map([
+            ["forgetfulness", "forgor"],
+            ["heehee", "hoho"],
+            ["are you winning", "son"],
+        ]);
+        expect(parseOverridesInput()).toEqual(expected);
+    });
+
+    test("no seperator", () => {
+        getMultilineInputMock.mockImplementation((name: string): string[] => {
+            expect(name).toBe("overrides");
+            return [
+                "forgetfulness forgor",
+            ];
+        });
+        expect(parseOverridesInput()).toEqual(new Map());
+    });
+
+    test("mixed valid & invalid", () => {
+        getMultilineInputMock.mockImplementation((name: string): string[] => {
+            expect(name).toBe("overrides");
+            return ["forgetfulness forgor", "heehee: hoho"];
+        });
+        expect(parseOverridesInput()).toEqual(new Map([["heehee", "hoho"]]));
     });
 });
